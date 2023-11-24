43e77a2a7ff7af258c8942c89def94b0c5ccccf7
Sam Zilli <sam@Sams-iMac.local>
2023-11-24 11:56:18 -0500

    successfully retrieving country and income info in update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// canvas and margin sizes
 const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
@@ -5,10 +6,12 @@ const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
 // increments based on what year is retrieved from dataset
 let year = 0
 
+// retrieving svg element from html and adding margins
 const svg = d3.select("#chart-area").append("svg")
  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
+// adding internal element inside margin sizes for graph
 const g = svg.append("g")
  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
@@ -36,13 +39,16 @@ const yLabel = g.append("text")
 //  .paddingInner(0.3)
 //  .paddingOuter(0.2)
 
-// x axis will be logarithmic
+// x axis will be logarithmic, default base 10
 const x = d3.scaleLog()
-	.domain([0,100])
-	.range([0,40000])
+	.domain([0, 40000]) // adjust this after testing
+	.range([0,40000]) // the range of tick marks displayed
 
 const y = d3.scaleLinear()
- .range([HEIGHT, 0])
+	// ages range from 0 to 90 years old
+	.domain( [0, 90] )
+	// reversing the range, height is the minimum value
+	.range([HEIGHT, 0])
 
 const xAxisGroup = g.append("g")
  .attr("class", "x axis")
@@ -51,6 +57,21 @@ const xAxisGroup = g.append("g")
 const yAxisGroup = g.append("g")
  .attr("class", "y axis")
 
+ const xAxisCall = d3.axisBottom(x)
+ xAxisGroup.call(xAxisCall)
+	 .selectAll("text")
+	  .attr("cy", "10")
+	  .attr("cx", "-5")
+	  .attr("text-anchor", "end")
+	  .attr("transform", "rotate(-40)")
+
+
+  const yAxisCall = d3.axisLeft(y)
+		.ticks(10)
+		.tickFormat(d => d + " years")
+   yAxisGroup.call(yAxisCall)
+
+// data retrieval and binding from json
 d3.json("data/data.json").then(function(data){
  	d3.interval(() => {
 		// iterating through each of the objects in the data (each is a year)
@@ -61,10 +82,13 @@ d3.json("data/data.json").then(function(data){
 		else {
 			year = 0;
 		}
+		// data has an object for each year, inside that year
+		// another object for each country
 		const dataPerYear = data[year]["countries"]
+		// for each year, update by passing in all the country objects
 		update(dataPerYear)
 
- 	}, 1000)
+ 	}, 100000)
 	// console.log(data[year])
 	// console.log("______")
 	// console.log(data[year].countries)
@@ -74,11 +98,15 @@ d3.json("data/data.json").then(function(data){
 	// console.log(dataPerYear)
 
 	// the initial call to load the data
-	update(data)
+	// console.log(data[year]["countries"])
+	update(data[year]["countries"])
 })
 
 function update(data) {
 	const t = d3.transition().duration(750);
+	data.map ( (d) =>
+		console.log(d["country"] + " " + d["income"])
+		)
 
 	// data.map( (element) => {
 	// 	if(element["continent"] == "asia") {
@@ -92,47 +120,32 @@ function update(data) {
 	// data.map( (d) => d.map( (c) => console.log(c.income)))
 
  
- x.domain(data.map(d => d["country"]))
- y.domain( [0, 90] )
+ 	// x.domain(data.map(d => d["country"]))
 
 
- const xAxisCall = d3.axisBottom(x)
- xAxisGroup.transition(t).call(xAxisCall)
-   .selectAll("text")
-     .attr("cy", "10")
-     .attr("cx", "-5")
-     .attr("text-anchor", "end")
-     .attr("transform", "rotate(-40)")
+
+ // JOIN new data with old elements. For each country, make a circle
+// 	const circles = g.selectAll("circle")
+//     	.data(data, (d) => d["country"]) 
+
+//  // EXIT old elements not present in new data.
+// //  circles.exit()
+// //    .attr("fill", "red")
+// //    .transition(t)
+// //      .attr("height", 0)
+// //      .attr("cy", y(0))
+// //      .remove()
 
 
- const yAxisCall = d3.axisLeft(y)
-   .ticks(10)
-   .tickFormat(d => d + " years")
-   yAxisGroup.transition(t).call(yAxisCall)
-
-
- // JOIN new data with old elements.
- const circles = g.selectAll("circle")
-   .data(data, d => d["country"]) 
-
- // EXIT old elements not present in new data.
- circles.exit()
-   .attr("fill", "red")
-   .transition(t)
-     .attr("height", 0)
-     .attr("cy", y(0))
-     .remove()
-
-
- // ENTER new elements present in new data...
- circles.enter().append("circle")
-   .attr("fill", "grey")
-   .attr("cy", y(0))
-   .attr("height", 0)
-   // AND UPDATE old elements present in new data.
-   .merge(circles)
-   .transition(t)
-     .attr("cx", (d) => x(d["income"]))
-     .attr("cy", d => y(d["life_exp"]))
-	 .attr("r", x.bandwidth)
+// //  // ENTER new elements present in new data...
+// //  circles.enter().append("circle")
+// //    .attr("fill", "grey")
+// //    .attr("cy", y(0))
+// //    .attr("height", 0)
+// //    // AND UPDATE old elements present in new data.
+// //    .merge(circles)
+// //    .transition(t)
+// //      .attr("cx", (d) => x(Number(d["income"])))
+// //      .attr("cy", d => y(Number(d["life_exp"])))
+// // 	 .attr("r", x.bandwidth)
 }
